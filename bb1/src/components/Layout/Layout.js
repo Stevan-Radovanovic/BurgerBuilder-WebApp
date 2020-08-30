26fdc097c1127a6361e5fba6361346e3d9c8e634
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-08-30 14:31:52 +0200

    Added SideDrawer and Backdrop logic

--- bb1/src/components/Layout/Layout.js
+++ bb1/src/components/Layout/Layout.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import css from './Layout.module.css';
+import React, { Component } from 'react';
+import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = (props) => (
-  <React.Fragment>
-    <Toolbar />
-    <SideDrawer />
-    <main className={css.Content}>{props.children}</main>
-  </React.Fragment>
-);
+class Layout extends Component {
+  state = {
+    sideDrawerVisible: true,
+  };
+
+  sideDrawHandler = () => {
+    const visible = this.state.sideDrawerVisible;
+    this.setState({ sideDrawerVisible: !visible });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <Toolbar />
+        <SideDrawer
+          clicked={this.sideDrawHandler}
+          open={this.state.sideDrawerVisible}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Layout;
--- bb1/src/components/Navigation/SideDrawer/SideDrawer.js
+++ bb1/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import classes from './SideDrawer.module.css';
 import NavItems from '../NavItems/NavItems';
+import Backdrop from '../../Backdrop/Backdrop';
 
 const SideDrawer = (props) => {
+  let attached = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attached = [classes.SideDrawer, classes.Open];
+  }
+
   return (
-    <div className={classes.SideDrawer}>
-      <nav>
-        <p>
-          <strong>Side Menu</strong>
-        </p>
-        <NavItems />
-      </nav>
-    </div>
+    <React.Fragment>
+      <Backdrop clicked={props.clicked} />
+      <div className={attached.join(' ')}>
+        <nav>
+          <p>
+            <strong>Side Menu</strong>
+          </p>
+          <NavItems />
+        </nav>
+      </div>
+    </React.Fragment>
   );
 };
 
