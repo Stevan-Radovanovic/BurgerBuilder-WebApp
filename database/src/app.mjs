e44672fafe724bdb457992ab6d7242bf4e802f9b
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-09-01 14:34:43 +0200

    Added routes and allow middleware

--- database/src/app.mjs
+++ database/src/app.mjs
@@ -7,6 +7,19 @@ import OrderRoutes from './routes/order-routes.mjs';
 
 const app = Express();
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, PUT, OPTIONS'
+  );
+  next();
+});
+
 app.use(Body.json());
 
 app.use('/burger/orders', OrderRoutes);
--- database/src/controllers/order-controller.mjs
+++ database/src/controllers/order-controller.mjs
@@ -1,7 +1,27 @@
 import Order from '../models/order-model.mjs';
 
-const getOrders = async (req, res, next) => {};
+const getOrders = async (req, res, next) => {
+  try {
+    const result = await Order.findAll();
+    res.json({ orders: result });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const postOrder = async (req, res, next) => {};
+const postOrder = async (req, res, next) => {
+  try {
+    const result = await Order.create({
+      saladCount: req.body.saladCount,
+      meatCount: req.body.meatCount,
+      baconCount: req.body.baconCount,
+      cheeseCount: req.body.cheeseCount,
+      price: req.body.price,
+    });
+    res.json({ message: 'New order created', order: result });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export { postOrder, getOrders };
