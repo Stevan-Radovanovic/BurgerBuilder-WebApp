194e42476475ebe8b80c4fec3f628419b0ab8b87
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-09-03 19:42:31 +0200

    Added intial routing setup

--- bb1/src/App.js
+++ bb1/src/App.js
@@ -2,14 +2,22 @@ import React, { Component } from 'react';
 import Layout from './containers/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import {
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom/cjs/react-router-dom.min';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Switch>
+            <Route path="/burger" component={BurgerBuilder} />
+            <Route path="/checkout" component={Checkout} />
+            <Redirect to="/burger" from="/" exact />
+          </Switch>
         </Layout>
       </div>
     );
