8a2f5723d21cad5437059c379f0a9c2e991c3fcd
Stevan-Radovanovic <stevan.radovanovic@mmklab.org>
2020-09-05 22:47:10 +0200

    Connected fetching orders from the db

--- bb1/src/components/Order/Order.js
+++ bb1/src/components/Order/Order.js
@@ -1,17 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Order.module.css';
 
-class Order extends Component {
-  render() {
-    return (
-      <div className={classes.Order}>
-        <p>Ingredients: No Ingredients</p>
-        <p>
-          Total Price: <strong>A LOT</strong>
-        </p>
-      </div>
-    );
-  }
-}
+const Order = (props) => {
+  return (
+    <div className={classes.Order}>
+      <h4>Order no. {props.order.id}</h4>
+      <p>Salad Count: {props.order.saladCount}</p>
+      <p>Meat Count: {props.order.meatCount}</p>
+      <p>Bacon Count: {props.order.baconCount}</p>
+      <p>Cheese Count: {props.order.cheeseCount}</p>
+      <p>
+        Total Price: <strong>{props.order.price}</strong>
+      </p>
+    </div>
+  );
+};
 
 export default Order;
--- bb1/src/containers/Orders/Orders.js
+++ bb1/src/containers/Orders/Orders.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios/axios-orders';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+  };
+
+  async componentDidMount() {
+    try {
+      const result = await axios.get('/orders');
+      console.log(result.data.orders);
+      this.setState({ orders: result.data.orders });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   render() {
-    return (
-      <div>
-        <Order />
-        <Order />
-      </div>
-    );
+    const transformedOrders = this.state.orders.map((order) => {
+      return <Order key={order.id} order={order} />;
+    });
+
+    return <div>{transformedOrders}</div>;
   }
 }
 
